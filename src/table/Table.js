2f184b97556ca1b68269b5c415bed7fa03b33666
a_shinenkov <a.shinenkov@cek.ru>
2021-04-26 19:32:59 +0300

    fix

--- src/table/Table.js
+++ src/table/Table.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './styles.scss';
 
-import Comment from './comment';
 import DataTable from './tableData';
 import Pagination from './pagination';
 import { TableState } from './context/table/TableState';
 import { BuildState } from './context/build/buildState';
+import Test from './test';
 
 const Table = () => {
   const handleClearHide = () => {
@@ -15,24 +15,27 @@ const Table = () => {
     localStorage.setItem('comment', null);
   };
   return (
-    <TableState>
-      <BuildState>
-        <div className="container">
-          <div className="paper">
-            <Pagination />
+    <>
+      <TableState>
+        <BuildState>
+          <div className="container">
+            <div className="paper">
+              <Pagination />
+            </div>
+            <DataTable />
+            {/* <div className="paper">
+              <button onClick={handleClearHide}>
+                Отчистить HIDE
+              </button>
+              <button onClick={handleClearComment} style={{marginLeft: 16}}>
+                Отчистить COMMENT
+              </button>
+            </div> */}
           </div>
-          <DataTable />
-          {/* <div className="paper">
-            <button onClick={handleClearHide}>
-              Отчистить HIDE
-            </button>
-            <button onClick={handleClearComment} style={{marginLeft: 16}}>
-              Отчистить COMMENT
-            </button>
-          </div> */}
-        </div>
-      </BuildState>
-    </TableState>
+        </BuildState>
+      </TableState>
+      <Test />
+    </>
   );
 };
 
---
+++ src/table/test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Test extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        null: 0,
+    };
+  }
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+       
+      </div>
+    );
+  }
+}
+
+export default Test;
